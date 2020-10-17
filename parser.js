728527544687dc65baf228e0ccc4247570d1e799
Gargi <gargi@Gargis-MacBook-Air.local>
2020-10-17 12:03:31 +0530

    7

---
+++ parser.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let jsonData = fs.readFileSync('./mochawesome-report/mochawesome.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(data)
+  });
+console.log(JSON.stringify(jsonData));
